f58a80a00a5795045234746f00ab89d2d67cfb91
Andrey Larin <lestex@gmail.com>
2020-05-06 19:38:16 -0400

    Ticket: Delete Comments

--- mflix/db.py
+++ mflix/db.py
@@ -368,7 +368,7 @@ def update_comment(comment_id, user_email, text, date):
     # Use the user_email and comment_id to select the proper comment, then
     # update the "text" and "date" of the selected comment.
     response = db.comments.update_one(
-        {"_id": comment_id, "email": user_email},
+        {"_id": ObjectId(comment_id), "email": user_email},
         {"$set": {"text": text, "date": date}}
     )
 
@@ -390,7 +390,9 @@ def delete_comment(comment_id, user_email):
 
     # TODO: Delete Comments
     # Use the user_email and comment_id to delete the proper comment.
-    response = db.comments.delete_one( { "_id": ObjectId(comment_id) } )
+    response = db.comments.delete_one(
+        {"_id": ObjectId(comment_id), "email": user_email}
+    )
     return response
 
 

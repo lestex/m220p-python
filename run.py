c096c5f0a9d42663796523e197e15af1889971cb
Andrey Larin <lestex@gmail.com>
2020-06-27 23:10:37 -0400

    updates entrypoint

--- run.py
+++ run.py
@@ -7,8 +7,9 @@ import configparser
 config = configparser.ConfigParser()
 config.read(os.path.abspath(os.path.join(".ini")))
 
+app = create_app()
+
 if __name__ == "__main__":
-    app = create_app()
     app.config['DEBUG'] = True
     app.config['MFLIX_DB_URI'] = config['PROD']['MFLIX_DB_URI']
     app.config['MFLIX_NS'] = config['PROD']['MFLIX_NS']
